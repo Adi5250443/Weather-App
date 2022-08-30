a3b4913f3c0d9afb8b4a7f9f4c6652f439502373
Aditya Prakash <adityajaiswal735@gmail.com>
2022-08-31 00:21:21 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+
+
+
+function App() {
+  return (
+    <div>
+      
+<Header/>
+    
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+<link href="https://fonts.googleapis.com/css2?family=Anek+Odia:wght@300&family=Roboto+Mono:wght@200&family=Roboto+Slab&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet"></link>
+
+
+ReactDOM.render(
+  
+    <App />
+  ,
+  document.getElementById('root')
+);
+
+
+
